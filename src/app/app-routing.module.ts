a4bce96686c29407307516bdea949fac9cc44a31
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-06-05 20:48:21 +0200

    - Revise the HeroDetailComponent - Add HeroService.getHero() - Find the way back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HeroListComponent} from "./hero-list/hero-list.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {SelectedHeroComponent} from "./selected-hero/selected-hero.component";
 
 const routes: Routes = [
   {path:'heroes', component: HeroListComponent},
   {path:'dashboard', component: DashboardComponent},
+  {path:'detail/:id', component: SelectedHeroComponent},
   {path:'', redirectTo:'/dashboard', pathMatch: 'full'}
 ];
 
--- src/app/selected-hero/selected-hero.component.ts
+++ src/app/selected-hero/selected-hero.component.ts
@@ -1,11 +1,34 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from "../app.hero";
+import {HeroService} from "../hero.service";
+import {ActivatedRoute, Params} from "@angular/router";
+import 'rxjs/add/operator/switchMap';
+import {Location} from '@angular/common';
+
+
 
 @Component({
   selector: 'app-selected-hero',
   templateUrl: './selected-hero.component.html',
   styleUrls: ['./selected-hero.component.css']
 })
-export class SelectedHeroComponent{
+export class SelectedHeroComponent implements OnInit {
   @Input() selectedHero:Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.selectedHero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
 }
